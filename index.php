110a7b3940ba76b458a5789db282f198ea6a93ea
andrkyn <andrhil14@gmail.com>
2017-02-01 11:12:24 +0200

    fix:(add  comment from andr1)

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 require_once("Observer.php");
 require_once("Observable.php");
 
-// my commit 1
+// my commit 1,
+// my comment 2
 $ul = new UserList();
 $ul->addObserver( new UserListLogger() );
 $ul->addCustomer( "Andrey" );
