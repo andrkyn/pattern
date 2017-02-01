5fce647a07804bf120a920179be5107b30b6d090
andrkyn <andrhil14@gmail.com>
2017-02-01 12:30:36 +0200

    fix from win_user

--- Observer.php
+++ Observer.php
@@ -9,7 +9,7 @@ interface IObserver
 {
     function onChanged( $sender, $args );
 }
-
+// comment from win_user
 class UserListLogger implements IObserver
 {
     public function onChanged( $sender, $args )
